2d4b879aef425adbadd174e1b6dec9ca4d17053c
hkldtrinanda <92398870+testerwebsite@users.noreply.github.com>
2021-11-16 17:08:43 +0700

    pemira

---
+++ tailwind.config.js
@@ -0,0 +1,31 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        Vintage: {
+          default: "#CEE5D0",
+          1: "#F3F0D7",
+          2: "#D8B384",
+          3: "#5E454B",
+          4: "#D9CDBA",
+          5: "#F0E3D0",
+          6: "#A28F71",
+          7: "#716358",
+          8: "#D3BD9A",
+          9: "#FB4172",
+          10: "#009690",
+          11: "#F7DCAD",
+        },
+        Red: {
+          10: "#810D24",
+        },
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
